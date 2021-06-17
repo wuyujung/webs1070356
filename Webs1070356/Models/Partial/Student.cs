1e6a1e31e43f1368099d7731362f66153bd353e8
周世哲 <shihche8917@gmail.com>
2021-06-17 20:37:13 +0800

    Students

---
+++ Webs1070356/Models/Partial/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Webs1070356.Models
+{
+    [MetadataType(typeof(StudentMetadata))]
+    public partial class Student
+    {
+    }
+
+
+
+
+    public class StudentMetadata
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "請填寫姓名")]
+        [Display(Name = "姓名")]
+        [StringLength(5, MinimumLength =2, ErrorMessage = "請輸入5~10個字")]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "請填寫學號")]
+        [Display(Name = "學號")]
+        [StringLength(10, ErrorMessage = "請輸入1~10個字")]
+        public string Number { get; set; }
+
+
+        [Required(ErrorMessage = "請填寫 Email")]
+        [Display(Name = "Email")]
+        [EmailAddress]
+        
+        public string Email { get; set; }
+        
+
+    }
+}
